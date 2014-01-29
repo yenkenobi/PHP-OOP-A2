3652c9f02711f13a3ed70f1a278c2b6bd0d70b87
theyhoang <theyhoang@gmail.com>
2014-01-28 19:06:08 -0800

    add-song-process done

--- classes/Song.php
+++ classes/Song.php
@@ -6,11 +6,48 @@
  * Time: 6:23 PM
  */
 
+
+
 class Song {
-    protected $pdo;
+    protected $title;
+    protected $artistId;
+    protected $genreId;
+    protected $price;
+    protected $sql;
+    protected $songs;
 
     public function __construct($pdo) {
         $this->pdo = $pdo;
+    }
+
+    public function setTitle($title) {
+        $this->title = $title;
+    }
+
+    public function setArtistId($artistId) {
+        $this->artistId = $artistId;
+    }
+
+    public function setGenreId($genreId) {
+        $this->genreId = $genreId;
+    }
+
+    public function setPrice($price) {
+        $this->price = $price;
+    }
+
+    public function getTitle() {
+        return $this->title;
+    }
+
+    public function getId() {
+        return $this->pdo->lastInsertId();
+    }
+
+    public function save() {
+        $this->sql = "INSERT INTO songs(title,artist_id,genre_id,price,play_count) VALUES ('$this->title',$this->artistId,$this->genreId,$this->price,0)";
+        $statement = $this->pdo->prepare($this->sql);
+        return $statement->execute();
     }
 
 } 
